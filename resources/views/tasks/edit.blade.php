0cffb63cd3c664515a45d7827a3cf597f924d39c
jiro.kirameki <ec2-user@ip-172-31-4-167.us-east-2.compute.internal>
2018-06-01 01:51:49 +0000

    task bootstrap design

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,20 +2,27 @@
 
 @section('content')
 
-    <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
+    <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
+    <div class="row">
+    　　<div class="col-xs-6">
     
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+    　　　　{!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        　　　　<div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+    　  　　　　　</div>
         
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
+        　　　　<div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+　　　　        </div>
         
+        　　　　{!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
-        {!! Form::submit('更新') !!}
+    　　　　　　{!! Form::close() !!}
 
-    {!! Form::close() !!}
-
+　　　　</div>
+    </div>
 @endsection
